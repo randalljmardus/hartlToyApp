983b0c6bbc19aa6a1ab27730913a4969ccde958d
Randall Mardus <randalljmardus@gmail.com>
2015-09-03 02:15:46 +0000

    Toy App Exercises

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ActiveRecord::Base
 
   belongs_to :user
   
-    validates :content, length: { maximum: 140 }
-
+    validates :content, length: { maximum: 140 },
+                        presence: true
+                        
 end
